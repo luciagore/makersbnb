a5d311f8d33a6bab2bf43916103482e7ffa15e4e
luciagore <lucy.gore@hotmail.co.uk>
2018-06-18 14:24:57 +0100

    rakefile setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,19 @@
 ENV['ENVIROMENT'] = 'test'
+ENV['RACK_ENV'] = 'test'
+
+Rake.application.load_rakefile
+
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'rake'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 Capybara.app = BNBmanager
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    Rake::Task['test_database_setup'].execute
+  end
+
+end
