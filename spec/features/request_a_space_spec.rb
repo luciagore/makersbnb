a1ba9b3dc1d23cd7b67fdadaa4f0d1ff34bffbd4
Benjamin Tomkins <bentomkins@hotmail.com>
2018-06-19 12:58:39 +0100

    wrote feature test for second user story

---
+++ spec/features/request_a_space_spec.rb
@@ -0,0 +1,15 @@
+feature 'list a space' do
+    scenario 'a user can request a space using email details' do
+
+        visit('/request')
+
+        fill_in('body', with: 'some message request')
+        fill_in('email', with: 'test@email.com')
+
+        click_button('Request')
+
+        expect(page).to have_content("some message request")
+        expect(page).to have_content("test@email.com")
+
+    end
+end
