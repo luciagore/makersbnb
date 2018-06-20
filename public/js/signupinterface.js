bfd08198b443ea57a7477018c3ba199ee7e6826d
Laura Petre <laura.d.petre@gmail.com>
2018-06-20 10:53:05 +0100

    added name field, wrote feature test for signin up

--- public/js/signupinterface.js
+++ public/js/signupinterface.js
@@ -3,11 +3,13 @@ $(document).ready(function(){
   $('#sign up').one('click', function(){
     let email = $('#email').val();
     let password = $('#password').val();
+    let name = $('#name').val();
     let username = $('#username').val();
 
     let xhr = $.post('/signup', {
       email: email,
       password: password,
+      name: name;
       username: username
     });
 
