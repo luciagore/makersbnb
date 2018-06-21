26b84fe1f6a78da9e47663231345049ea7b7cb03
Khie Biggs <khiebigzy@gmail.com>
2018-06-21 18:16:05 +0100

    remove book now link when not signed in

--- public/js/indexinterface.js
+++ public/js/indexinterface.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
   let xhr = $.post('/getusers');
   xhr.done(function(response){
     for(let i in response){
-      $('#listings').append("<li>" +
-      response[i].name_of_space + " - " +
-      response[i].description + " by: " +
-      response[i].email + "<a href = '/newrequest?space_id=" +
-      response[i].id +"'>book now!</a></li>");
+      let message = "<li>" + response[i].name_of_space + " - " + response[i].description + " by: " + response[i].email
+      if($('#signedin').val()){
+        message = message + "<a href = '/newrequest?space_id=" + response[i].id +"'>book now!</a></li>"
+      }
+      $('#listings').append(message);
     }
   });
 });
