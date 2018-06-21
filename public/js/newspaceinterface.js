1c82091aabc210907ec7386343b94cfed6998185
luciagore <lucy.gore@hotmail.co.uk>
2018-06-21 11:52:18 +0100

    added user_id and price per night to interface

--- public/js/newspaceinterface.js
+++ public/js/newspaceinterface.js
@@ -8,16 +8,20 @@ $(document).ready(function() {
       let name_of_space = $('#name_of_space').val();
       let email = $('#email').val();
       let description = $('#description').val();
+      let price = $('#price').val();
+      let user_id = $('#user_id').val();
 
       let xhr = $.post('/new',
       {
         name_of_space: name_of_space,
         email: email,
-        description: description
+        description: description,
+        price: price,
+        user_id: user_id
       });
 
       xhr.done(function(response){
-        $('#response').append("Made a new listing:<br>email: " + response.email + "<br>name of space: " + response.name_of_space + "<br>description: " +response.description);
+        $('#response').append("Made a new listing:<br>email: " + response.email + "<br>name of space: " + response.name_of_space + "<br>price: " + response.price + "<br>description: " +response.description);
       });
     });
 
