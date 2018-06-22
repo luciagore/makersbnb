d2bc9d3284bdc3411cc1588c5a04ea80a4702070
Khie Biggs <khiebigzy@gmail.com>
2018-06-22 11:53:37 +0100

    add upload image functionality

--- spec/features/list_a_space_spec.rb
+++ spec/features/list_a_space_spec.rb
@@ -8,11 +8,12 @@ feature 'list a space' do
     fill_in('name_of_space', with: "Lucys house")
     fill_in('email', with: 'lucy@example.com')
     fill_in('description', with: 'Home for rent!')
-    fill_in('price', with: '40')
-
-    click_button('List my space')
+    fill_in('price_per_night', with: '40')
+    attach_file('file', File.absolute_path('./public/images/head.jpg'))
+    click_button('Submit')
     expect(page).to have_content("Lucys house")
     expect(page).to have_content('Home for rent!')
     expect(page).to have_content('lucy@example.com')
+    File.delete(File.absolute_path('./public/userimages/1head.jpg')) if File.exist?(File.absolute_path('./public/userimages/1head.jpg'))
   end
 end
