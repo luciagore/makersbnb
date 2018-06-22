98513fa1f8af77e3d1d74e2364f0cae66a2dd253
Khie Biggs <khiebigzy@gmail.com>
2018-06-22 12:57:50 +0100

    change image sizes to 300x200

--- public/js/indexinterface.js
+++ public/js/indexinterface.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   let xhr = $.post('/getusers');
   xhr.done(function(response){
     for(let i in response){
-      let message1 = "<div class='p-2 item-hl'><div class='card' style='width: 18rem'><img class='card-img-top img-fluid' src='userimages/" + response[i].image_name +"' alt='' class = 'user_image'><div class='card-body'><h4 class='card-title'>"
+      let message1 = "<div class='p-2 item-hl'><div class='card' style='width: 18rem'><img class='card-img-top img-fluid' style = 'height:200px; width:300px;' src='userimages/" + response[i].image_name +"' alt='' class = 'user_image'><div class='card-body'><h4 class='card-title'>"
       + response[i].name_of_space +
       "</h4><p class='card-text'>"
       + response[i].description +
@@ -16,43 +16,3 @@ $(document).ready(function(){
     }
   });
 });
-
-
-
-
-// "use strict";
-// $(document).ready(function(){
-//   let xhr = $.post('/getusers');
-//   xhr.done(function(response){
-//     for(let i in response){
-//       let message = "<div class='p-2 item-hl'><div class='card' style='width: 18rem'><img class='card-img-top img-fluid' src='https://source.unsplash.com/random/300x200' alt=''><div class='card-body'><h4 class='card-title'>"
-//       + response[i].name_of_space +
-//       "</h4><p class='card-text'>"
-//       + response[i].description +
-//       "</p><p class='card-text'>£50 per night</p><p class='card-text'>"
-//       + response[i].email +
-//       "</p></div></div></div>"
-//       if($('#signedin').val()){
-//         message = message + "<a href = '/newrequest?space_id=" + response[i].id +"'>book now!</a>"
-//       }
-//       $('#listings').append(message);
-//     }
-//   });
-// });
-//
-//
-
-
-// "use strict";
-// $(document).ready(function(){
-//   let xhr = $.post('/getusers');
-//   xhr.done(function(response){
-//     for(let i in response){
-//       let message = "<li>" + response[i].name_of_space + " - " + response[i].description + " by: " + response[i].email
-//       if($('#signedin').val()){
-//         message = message + "<a href = '/newrequest?space_id=" + response[i].id +"'>book now!</a></li>"
-//       }
-//       $('#listings').append(message);
-//     }
-//   });
-// });
